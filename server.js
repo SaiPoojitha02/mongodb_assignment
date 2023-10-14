8266462f00ace4c666989e0a9f957ddb208ed96e
SaiPoojitha02 <143846108+SaiPoojitha02@users.noreply.github.com>
2023-10-14 11:36:16 -0400

    public folder not working

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.get('/budget', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log('API served at http://localhost:${port}');
+    console.log(`API served at http://localhost:${port}`);
 });
